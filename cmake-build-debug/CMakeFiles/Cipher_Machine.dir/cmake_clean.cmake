976cefa54c2ca77776f6d319d891f2c41bd3754a
Gregory Maldonado <gmaldona@oswego.edu>
2021-08-31 02:11:31 -0400

    Setup

---
+++ cmake-build-debug/CMakeFiles/Cipher_Machine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cipher_Machine.dir/main.c.o"
+  "Cipher_Machine"
+  "Cipher_Machine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Cipher_Machine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Cipher_Machine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cipher_Machine.
+# This may be replaced when dependencies are built.
